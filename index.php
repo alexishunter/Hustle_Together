c69db951f5149aa449df1b8837c1ae6674628853
Alexis Hunter <alexisprestonhunter@gmail.com>
2014-06-01 20:36:24 -0500

    2nd commit

--- index.php
+++ index.php
@@ -27,17 +27,40 @@ if (mysqli_connect_errno()) {
   ?>
 <div id="content">
 
-
-<form action="insert.php" method="post">
-<h1>Where are you hustlin'?</h1>
+	<div id="find">
+	<form action="insert.php" method="post">
+	<h1>Find who you're looking for</h1>
+		<h3>Where are you hustlin'?</h3>
 		 <input type="text" name="location" value="ie. Dallas, Austin, San Fran...">
-<h1>What's your hustle?</h1> 
+		<h3>What's your hustle?</h3> 
 		 <input type="text" name="pursuit" value="ie. Styling, Modeling, Sewing...">
 		 <br><br><br>
 		 <input type="submit">
-</form>
+	</form>
+	</div>
+	
+	<div id="help">
+	<form action=# method #>
+	<h1> Help others find you</h1>
+		<h3>What's your name?</h3>
+		<input type="text" name="name" value="ie. Jimi Hendrix">
+		<h3>Where are you hustlin'?</h3>
+		<input type="text" name="location" value="ie. Dallas, Austin, San Fran...">
+		<h3>What's your hustle?</h3>
+		<input type="text" name="pursuit" value="ie. Styling, Modeling, Sewing...">
+		<h3>How can you be reached?</h3>
+		<input type="text" name="contact_info" value="ie. Email addy, Phone #, Twitter acct url">
+		<br><br><br>
+		<input type="submit">
+		<!--<h1>What do you look like?</h1>
+		insert picture-->
+	</form>
+	</div>
+
 </div>
-<div id="footer"> Made by <a href="https://twitter.com/ayeee_lexis/" target="_blank">Alexis Hunter</a> in her leopard sheeted bed. </div>
+		
+
+<!--<div id="footer"> Made by <a href="https://twitter.com/ayeee_lexis/" target="_blank">Alexis Hunter</a> in her leopard sheeted bed. </div>-->
 
 </body>
 </html>
